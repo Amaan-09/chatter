8250e34e6f4ee22e317ec61d27f39efaa2780826
Amaan-09 <79093439+Amaan-09@users.noreply.github.com>
2021-07-01 08:16:46 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -18,10 +18,27 @@ var firebaseConfig = {
     user_name=localStorage.getItem("user_name");
     document.getElementById("user_name1").innerHTML="Welcome! "+user_name;
 
+    function addRoom()
+    {
+          room_name= document.getElementById("room_name").value;
+
+          firebase.database().ref("/").child(room_name).update({
+                purpose : "adding room name"
+          });
+
+          localStorage.setItem("room_name", room_name);
+
+          window.location= "index.html";
+    }
+
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
-      
+
+
+       console.log("Room Name - " + Room_names);
+       row= "<div class='room_name' id="+Room_names+" onclick= 'redirectToRoomName(this.id)'>#"+ Room_names +"</div><hr>";
+       document.getElementById("output").innerHTML += row;
 
       
       });});}
@@ -29,4 +46,11 @@ getData();
 
 function logout() {
       window.location="index.html";
+}
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name",name);
+      window.location= "index.html";
 }
